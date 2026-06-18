8a799773d99dcf8a54ae505136ade8431acee6be
hieuliveregedit <hp03092007@gmail.com>
2026-06-19 01:52:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,89 @@
 function checkKey(){
-let key=document.getElementById("keyInput").value.toLowerCase();
+
+let key=document.getElementById("keyInput").value.toLowerCase().trim();
+
 let ok=false;
 
 for(let i=1;i<=100;i++){
-if(key==="phamhieu"+i) ok=true;
+if(key==="phamhieu"+i){
+ok=true;
+break;
+}
 }
 
 if(ok){
 loginBox.style.display="none";
 deviceBox.style.display="block";
 }else{
-alert("Sai key");
-}
+alert("Sai mật khẩu!");
 }
 
-function rand(a,b){
-return Math.floor(Math.random()*(b-a+1)+a);
+}
+
+function rand(min,max){
+return Math.floor(Math.random()*(max-min+1))+min;
 }
 
 function saveDevice(){
-let name=document.getElementById("deviceInput").value;
-if(!name) return;
 
-let data=JSON.parse(localStorage.getItem("d"))||{};
+let name=document.getElementById("deviceInput").value.trim();
+
+if(!name){
+alert("Nhập tên máy!");
+return;
+}
+
+deviceBox.style.display="none";
+scanBox.style.display="block";
+
+let p=0;
+
+let timer=setInterval(()=>{
+
+p++;
+
+progressBar.style.width=p+"%";
+percent.innerText=p+"%";
+
+if(p>=100){
+
+clearInterval(timer);
+
+scanBox.style.display="none";
+
+showResult(name);
+
+}
+
+},30);
+
+}
+
+function showResult(name){
+
+let data=JSON.parse(localStorage.getItem("devices"))||{};
 
 if(!data[name]){
+
 data[name]={
-look:rand(150,190),
-red:rand(150,190),
-scope2:rand(150,190),
-scope4:rand(150,190),
-awm:rand(150,190),
-free:rand(150,190),
-fire:rand(33,50)
+
+look:rand(190,200),
+red:rand(150,200),
+scope2:rand(150,200),
+scope4:rand(150,200),
+awm:rand(150,200),
+free:rand(150,200),
+fire:rand(40,60)
+
 };
-localStorage.setItem("d",JSON.stringify(data));
+
+localStorage.setItem("devices",JSON.stringify(data));
+
 }
 
 let s=data[name];
+
+deviceName.innerText="📱 "+name;
 
 look.innerText=s.look;
 red.innerText=s.red;
@@ -47,24 +93,54 @@ awm.innerText=s.awm;
 free.innerText=s.free;
 fire.innerText=s.fire;
 
-deviceBox.style.display="none";
 mainApp.style.display="block";
+
 }
 
 function newDevice(){
+
 mainApp.style.display="none";
 deviceBox.style.display="block";
+
 }
 
 function downloadConfig(){
 
-let txt=`VIP DPI PRO
-Tang do nhay
-Lien he: 0987124052`;
+const profile=`<?xml version="1.0" encoding="UTF-8"?>
 
-let blob=new Blob([txt]);
-let a=document.createElement("a");
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+"http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+
+<plist version="1.0">
+<dict>
+
+<key>PayloadType</key> <string>Configuration</string>
+
+<key>PayloadVersion</key> <integer>1</integer>
+
+<key>PayloadIdentifier</key> <string>com.phamhieu.profile</string>
+
+<key>PayloadUUID</key> <string>12345678-1234-1234-1234-123456789012</string>
+
+<key>PayloadDisplayName</key> <string>PhamHieu Device Profile</string>
+
+<key>PayloadDescription</key> <string>Profile demo</string>
+
+<key>PayloadOrganization</key> <string>PhamHieu Tech</string>
+
+<key>PayloadContent</key> <array/>
+
+</dict>
+</plist>`;
+
+const blob=new Blob([profile],{
+type:"application/x-apple-aspen-config"
+});
+
+const a=document.createElement("a");
+
 a.href=URL.createObjectURL(blob);
-a.download="vip.mobileconfig";
+a.download="PhamHieu.mobileconfig";
 a.click();
+
 }
