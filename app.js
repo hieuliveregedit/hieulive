0f64148ae4209cd4d53354a2f3b0e3d40a6343f0
hieuliveregedit <hp03092007@gmail.com>
2026-06-19 02:01:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
 function checkKey(){
 
-let key=document.getElementById("keyInput").value.toLowerCase().trim();
+let key=document
+.getElementById("keyInput")
+.value
+.toLowerCase()
+.trim();
 
 let ok=false;
 
 for(let i=1;i<=100;i++){
+
 if(key==="phamhieu"+i){
+
 ok=true;
 break;
+
 }
+
 }
 
 if(ok){
+
 loginBox.style.display="none";
 deviceBox.style.display="block";
+
 }else{
+
 alert("Sai mật khẩu!");
+
 }
 
 }
 
 function rand(min,max){
-return Math.floor(Math.random()*(max-min+1))+min;
+
+return Math.floor(
+Math.random()*(max-min+1)
+)+min;
+
 }
 
 function saveDevice(){
 
-let name=document.getElementById("deviceInput").value.trim();
+let name=document
+.getElementById("deviceInput")
+.value
+.trim();
 
 if(!name){
-alert("Nhập tên máy!");
+
+alert("Vui lòng nhập tên máy!");
 return;
+
 }
 
 deviceBox.style.display="none";
@@ -61,7 +82,10 @@ showResult(name);
 
 function showResult(name){
 
-let data=JSON.parse(localStorage.getItem("devices"))||{};
+let data=
+JSON.parse(
+localStorage.getItem("devices")
+)||{};
 
 if(!data[name]){
 
@@ -77,13 +101,17 @@ fire:rand(40,60)
 
 };
 
-localStorage.setItem("devices",JSON.stringify(data));
+localStorage.setItem(
+"devices",
+JSON.stringify(data)
+);
 
 }
 
 let s=data[name];
 
-deviceName.innerText="📱 "+name;
+deviceName.innerText=
+"📱 "+name;
 
 look.innerText=s.look;
 red.innerText=s.red;
@@ -100,47 +128,47 @@ mainApp.style.display="block";
 function newDevice(){
 
 mainApp.style.display="none";
+
 deviceBox.style.display="block";
+
+document
+.getElementById("deviceInput")
+.value="";
 
 }
 
 function downloadConfig(){
 
-const profile=`<?xml version="1.0" encoding="UTF-8"?>
+const content=
+`PHAMHIEU PRO CHIP A
 
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
-"http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+Trang thai: Da xac thuc
 
-<plist version="1.0">
-<dict>
+Phien ban: CHIP A
 
-<key>PayloadType</key> <string>Configuration</string>
+Tuong thich:
+iPhone / iPad
 
-<key>PayloadVersion</key> <integer>1</integer>
+Lien he Admin:
+0987124052`;
 
-<key>PayloadIdentifier</key> <string>com.phamhieu.profile</string>
-
-<key>PayloadUUID</key> <string>12345678-1234-1234-1234-123456789012</string>
-
-<key>PayloadDisplayName</key> <string>PhamHieu Device Profile</string>
-
-<key>PayloadDescription</key> <string>Profile demo</string>
-
-<key>PayloadOrganization</key> <string>PhamHieu Tech</string>
-
-<key>PayloadContent</key> <array/>
-
-</dict>
-</plist>`;
-
-const blob=new Blob([profile],{
-type:"application/x-apple-aspen-config"
-});
+const blob=new Blob(
+[content],
+{
+type:"text/plain"
+}
+);
 
 const a=document.createElement("a");
 
-a.href=URL.createObjectURL(blob);
-a.download="PhamHieu.mobileconfig";
+a.href=
+URL.createObjectURL(blob);
+
+a.download=
+"PhamHieu-Chip-A.txt";
+
 a.click();
+
+URL.revokeObjectURL(a.href);
 
 }
